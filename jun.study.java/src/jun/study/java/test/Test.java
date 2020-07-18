dca544876cfcda3422ae06ffbb76d671ceceaf2d
USER <USER@RedRooster>
2020-07-18 11:08:29 +0900

    First Git Test : jhj

---
+++ jun.study.java/src/jun/study/java/test/Test.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package jun.study.java.test;
+
+/**
+ * @author USER
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("==================================");
+
+	}
+
+}
