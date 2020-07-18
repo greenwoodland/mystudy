bd14f173d1136ef2ae03a45ad1d7db21eb7693e7
USER <USER@RedRooster>
2020-07-18 11:14:03 +0900

    Second git Test

---
+++ jun.study.java/src/jun/study/java/test/Test1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package jun.study.java.test;
+
+/**
+ * @author USER
+ *
+ */
+public class Test1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("My Test Project");
+
+	}
+
+}
